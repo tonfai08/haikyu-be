d6e035a4fe474d753aea253f064c53b355d39212
tonton159 <wittawat.s@deftdev.tech>
2025-04-11 03:14:45 +0700

    ีupdate

--- app.js
+++ app.js
@@ -38,7 +38,12 @@ app.use("/order", orderRouter);
 app.use("/customers", customerRouter);
 app.use("/version", versionRouter);
 app.use("/visit", visitRoutes);
-app.use("/api/floating", floatingRoute);
+app.use("/api/floating", (req, res, next) => {
+  if (req.method === "POST") {
+    return floatingLimiter(req, res, next); // ✅ ใช้ limiter เฉพาะ POST
+  }
+  next(); // ✅ อื่นๆ ไปต่อ
+}, floatingRoute);
 
 
 const port = process.env.PORT || 8080;
